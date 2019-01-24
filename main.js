ca5f1c059f784e2b472aa1c46ef0ba79e1ebeb6d
xwang3000 <xwang3000@gmail.com>
2019-01-24 13:27:07 -0800

    Add sample tea items info and details toggle effect

---
+++ main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $("#kirishima-details").slideToggle();
+  $("#kirishima").click(function(){
+    $("#kirishima-details").slideToggle(800);
+  });
+});
+
+$(document).ready(function(){
+  $("#15-details").slideToggle();
+
+  $("#15").click(function(){
+    $("#15-details").slideToggle(800);
+  });
+});
